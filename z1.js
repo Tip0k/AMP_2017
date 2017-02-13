cbe8af9032e946467deecd8e9dff53557250a8d9
Tip0k <a.tymofiev@gmail.com>
2017-02-13 15:50:18 +0200

    Update z1.js

--- z1.js
+++ z1.js
@@ -1,5 +1,24 @@
-let a = [5, 1, 6];
+/**
+ * Created by PEOPLE on 13.02.2017.
+ */
 
-for(let i = 0; (i + 2) < a.length(); i++) {
-   if((a[i] + a[i + 1]) == a[i + 2]) console.log("");
+var a = [5, 1, 6];
+var flag;
+
+console.log("Нехай є три числа: ", a[0], a[1], a[2]);
+
+if(a[0] === a[1] + a[2]) {
+    flag = a[0];
+} else if(a[1] === a[2] + a[0]) {
+    flag = a[1];
+} else if(a[2] === a[1] + a[0]) {
+    flag = a[2];
+} else {
+    flag = -1;
+}
+
+if(flag != -1) {
+    console.log("Число " + flag + " дорівнює сумі двох інших");
+} else {
+    console.log("Жодне число не дорівнює сумі двох інших");
 }
